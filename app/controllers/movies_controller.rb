84ed11d15eac050c1f0809aeb90705e5f64d3815
EC2 Default User <ec2-user@ip-172-31-34-23.us-east-2.compute.internal>
2018-09-28 02:48:14 +0000

    back

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -19,26 +19,28 @@ class MoviesController < ApplicationController
     if(params[:ratings])
       @checked_ratings=params[:ratings].keys
       session[:ratings]=@checked_ratings
-    else 
-      if @checked_ratings.nil? 
-        @checked_rating = @all_ratings 
-      else
+    else
       @checked_ratings = session[:ratings]
-      end
+      
     end
-    
     if(key)
        session[:key] = key 
     else
       key = session[:key]
     end
-   
-      
-    
-    @checked_ratings.each do |rating|
+    if @checked_ratings.nil? 
+      @all_ratings.each do |rating|
       params[rating] = true
+      end
+    else
+      @checked_ratings.each do |rating|
+      params[rating] = true
+      end  
     end
-
+    #end  
+   # @checked_ratings.each do |rating|
+    # params[rating] = true
+    #end  
     if key == 'title_header'
       @title_header = 'hilite'
       @movies = Movie.where(:rating=>@checked_ratings ).order('LOWER(title)')
